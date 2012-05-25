94e93800c31362ae62d39901983d82b7ea0af941
comonitos <comonitos@gmail.com>
2012-05-26 00:14:34 +0300

    IDEA CREATION UPDATE

--- 10ideas/IdeaDetailsViewController.h
+++ 10ideas/IdeaDetailsViewController.h
@@ -12,12 +12,14 @@
 {
     Idea *idea;
     BOOL edit;
+    BOOL isNew;
 }
 @property BOOL edit;
 @property (retain, nonatomic) IBOutlet UITextView *ideaText;
 @property (nonatomic, retain) Idea *idea;
 @property (retain, nonatomic) IBOutlet UIButton *rateButton;
+@property (unsafe_unretained, nonatomic) IBOutlet UIBarButtonItem *saveButton;
 
 - (IBAction) back;
-
+- (IBAction) save;
 @end
